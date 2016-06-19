d373ec117f8e98a1fdd972c3d3e611290f3f188e
Jair Trejo <jair@chegg.com>
2016-06-19 01:42:59 -0700

    Remove dependency on browser environment

--- src/core.js
+++ src/core.js
@@ -4,12 +4,10 @@ import Immutable from 'immutable';
 
 export function Madera(reducers, actionTypes){
     const action$ = new Bacon.Bus();
-    window.action$ = action$;
     action$.actionTypes = actionTypes;
 
     const state$ = Bacon.update(
         Immutable.Map(),
-        // Last defined has precedence
         ...reducers.map(reducer => reducer(action$))
                    .reduce((a, b) => a.concat(b))
     );
